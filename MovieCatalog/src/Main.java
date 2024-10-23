77294632ce25f15c9f1f7787f86f9dafc2b8c0af
BarbNcZ <70008211+BarbNcZ@users.noreply.github.com>
2024-10-23 19:18:36 -0300

    Add files via upload

---
+++ MovieCatalog/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Screen Match");
+        System.out.println("Movie: Kingsman: The Secret Service");
+
+        int year = 2014;
+        System.out.println("Release date: " + year);
+        boolean includedInPlan = true;
+
+    }
+}
